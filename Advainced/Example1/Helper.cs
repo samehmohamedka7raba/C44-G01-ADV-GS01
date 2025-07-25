36da908862a74c65811f2a9cf6757df09a53be96
Sameh Mohamed <126187468+samehmohamedka7raba@users.noreply.github.com>
2025-07-25 15:26:59 +0300

    Add project files.

---
+++ Advainced/Example1/Helper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advainced.Example1
+{
+    internal static class Helper
+    {
+        #region swap
+
+        public static void SWAP(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void SWAP(ref decimal x, ref decimal y)
+        {
+            decimal temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void SWAP(ref Point x, ref Point y)
+        {
+            Point temp = x;
+            x = y;
+            y = temp;
+        }
+        #endregion
+
+
+        #region SWAP - GENRIC
+        public static void SWAP<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+        #endregion
+
+
+
+
+
+
+    }
+}
---
+++ Advainced/Program.cs
@@ -0,0 +1,39 @@
+﻿using Advainced.Example1;
+
+namespace Advainced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region v1
+            int a = 10, b = 20;
+            Console.WriteLine($"a : {a}");
+            Console.WriteLine($"b : {b}");
+            Console.WriteLine($"After swap");
+            Helper.SWAP(ref a, ref b);
+            Console.WriteLine($"a : {a}");
+            Console.WriteLine($"b : {b}");
+
+            decimal x = 10.5m, y = 20.5m;
+            Console.WriteLine($"x : {x}");
+            Console.WriteLine($"y : {y}");
+            Console.WriteLine($"After swap");
+            Helper.SWAP(ref x, ref y);
+            Console.WriteLine($"x : {x}");
+            Console.WriteLine($"x : {y}");
+
+
+            Point p1 = new Point(1, 2);
+            Point p2 = new Point(3, 4);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine($"After swap");
+            Helper.SWAP(ref p1, ref p2);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            #endregion
+
+        }
+    }
+}
